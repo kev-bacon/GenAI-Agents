e1f6599d7a0274bc127a78d21c1b9c992cefb84b
kev-bacon <kevinnguyen6575@hotmail.com>
2023-12-26 01:32:34 +1100

    Added sidebar with buttons for: Home, Create and Settings

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components/navbar";
+import { Sidebar } from "@/components/sidebar";
 
 const RootLayout = ({
     children
@@ -7,7 +8,10 @@ const RootLayout = ({
 }) => {
     return (
         <div className ="h-full">
-            <Navbar/> 
+            <Navbar/>   
+            <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+                <Sidebar/> 
+            </div>
             <main className="md:pl-20 pt-16 h-full">
                 {children}
             </main>
