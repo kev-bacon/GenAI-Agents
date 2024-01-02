f240371db3fd509769245d79de4d5adfbef8d549
kev-bacon <kevinnguyen6575@hotmail.com>
2024-01-02 22:08:44 +1100

    Added Chat Headers and additional security measure (can't edit other people's agents)

--- app/(chat)/(routes)/chat/[chatId]/page.tsx
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -1,14 +1,49 @@
-// interface ChatIdPageProps {
-//     params: {
-//         chatId: string
-//     }
-// }
+import prismadb from "@/lib/prismadb";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ChatClient } from "./components/client";
 
-const ChatIdPage = () => {
+interface ChatIdPageProps {
+    params: {
+        chatId: string
+    }
+}
+
+const ChatIdPage = async ({
+    params
+}: ChatIdPageProps) => {
+    const {userId} = auth()
+
+    if (!userId) {
+        return redirectToSignIn
+    }
+
+    const companion = await prismadb.companion.findUnique({
+        where: {
+            id: params.chatId
+        },
+        include: {
+            messages: {
+                orderBy: {
+                    createdAt: "asc"
+                },
+                where: {
+                    userId
+                }
+            },
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    })
+
+    if (!companion) {
+        return redirect("/")
+    }
     return ( 
-        <div>
-            Hello Chat ID page 
-        </div>
+        <ChatClient companion= {companion} /> 
      );
 }
  
--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,52 +1,86 @@
-import prismadb from "@/lib/prismadb"
-import { auth, currentUser } from "@clerk/nextjs"
-import { NextResponse } from "next/server"
+import prismadb from "@/lib/prismadb";
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export async function PATCH(
-    req: Request,
-    {params}: {params: {companionId: string}}) {
+  req: Request,
+  { params }: { params: { companionId: string } }
+) {
+  try {
+    const body = await req.json();
+    const user = await currentUser();
+    const { src, name, description, instructions, seed, categoryId } = body;
+
+    // console.log("User object:", user);
+    // if (user) {
+    //     console.log("User ID:", user.id);
+    //     console.log("User First Name:", user.firstName);
+    // } else {
+    //     console.log("No user object returned");
+    // }
+    if (!params.companionId) {
+      return new NextResponse("Companion ID is required", { status: 400 });
+    }
+    if (!user || !user.id || !user.firstName) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (
+      !src ||
+      !name ||
+      !description ||
+      !instructions ||
+      !seed ||
+      !categoryId
+    ) {
+      return new NextResponse("Missing required fields", { status: 400 });
+    }
+
+    //TODO: Check for subscription
+    const companion = await prismadb.companion.update({
+      where: {
+        id: params.companionId,
+        userId: user.id
+      },
+      data: {
+        categoryId,
+        userId: user.id,
+        userName: user.firstName,
+        src,
+        name,
+        description,
+        instructions,
+        seed,
+      },
+    });
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log("[COMPANION_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { companionId: string } }
+) {
     try {
-        const body = await req.json() 
-        const user = await currentUser()
-        const { src, name, description, instructions, seed, categoryId} = body 
+        const { userId } = auth()
 
-        // console.log("User object:", user);
-        // if (user) {
-        //     console.log("User ID:", user.id);
-        //     console.log("User First Name:", user.firstName);
-        // } else {
-        //     console.log("No user object returned");
-        // }
-        if(!params.companionId) {
-            return new NextResponse("Companion ID is required", {status:400})
-        }
-        if(!user || !user.id || !user.firstName) {
-            return new NextResponse("Unauthorized", { status: 401})
+        if (!userId) {
+            return new NextResponse("Unauthorised", {status: 401})
         }
 
-        if (!src || !name || !description || !instructions || !seed || !categoryId) {
-            return new NextResponse("Missing required fields", { status: 400})
-        }
-
-        //TODO: Check for subscription
-        const companion = await prismadb.companion.update({
+        const companion = await prismadb.companion.delete({
             where: {
+                userId,
                 id: params.companionId
-            },
-            data: {
-                categoryId,
-                userId: user.id,
-                userName: user.firstName, 
-                src,
-                name, 
-                description,
-                instructions,
-                seed
             }
         })
+
         return NextResponse.json(companion)
-    } catch(error) {
-        console.log("[COMPANION_PATCH]", error)
+    } catch (error) {
+        console.log("[COMPANION_DELETE", error)
         return new NextResponse("Internal Error", { status: 500})
     }
 }
